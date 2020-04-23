8bad26f20445726fb4ef7e315d3b6aff03ac423b
Dylan Leong <dylanleong@yahoo.com>
2020-04-23 05:20:03 +0100

    model changes

--- models/person.js
+++ models/person.js
@@ -1,15 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Person = sequelize.define('Person', {
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-}, {});
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING
+  }, {});
   Person.associate = function(models) {
     // associations can be defined here
   };
